e74f41079c40741ae253886d4343e8928dcac8b6
Fernando Plaza <fernando.plaza@enzo.es>
2021-04-18 21:43:04 +0000

    Formulario hecho

--- helpdesk_fermatrix/models/helpdesk_ticket.py
+++ helpdesk_fermatrix/models/helpdesk_ticket.py
@@ -5,8 +5,43 @@ class HelpdeskTicket(models.Model):
     _description = 'Ticket'
 
     name = fields.Char(
-        string='Name')
+        string='Name',
+        required=True)
     description = fields.Text(
         string='Description')
     date = fields.Date(
         string='Date')
+
+    state = fields.Selection(
+        [('new', 'New'),
+        ('asignado', 'Asignado'),
+        ('proceso', 'En proceso'),
+        ('pendiente', 'Pendiente'),
+        ('resuelto', 'Resuelto'),
+        ('cancelado', 'Cancelado')],
+        string='State',
+        default='new')
+    
+    time = fields.Float(
+        string='Time')
+    
+    assigned = fields.Boolean(
+        string='Assigned',
+        readonly=True)
+
+    date_limit = fields.Date(
+        string='Date Limit')
+
+    action_corrective = fields.Html(
+        string='Corrective Action',
+        help='Describe corrective actions')
+    
+    action_preventive = fields.Html(
+        string='Preventive Action',
+        help='Describe preventive actions')
+    
+    
+    
+    
+    
+    
