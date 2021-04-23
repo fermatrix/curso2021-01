64a3175d31dc3a2e054ac3832643cc846a5c5230
Fernando Plaza <fernando.plaza@enzo.es>
2021-04-23 19:14:57 +0000

    Añadidos botones de acciones

--- helpdesk_fermatrix/models/helpdesk_ticket.py
+++ helpdesk_fermatrix/models/helpdesk_ticket.py
@@ -11,7 +11,6 @@ class HelpdeskTicket(models.Model):
         string='Description')
     date = fields.Date(
         string='Date')
-
     state = fields.Selection(
         [('new', 'New'),
         ('asignado', 'Asignado'),
@@ -21,27 +20,43 @@ class HelpdeskTicket(models.Model):
         ('cancelado', 'Cancelado')],
         string='State',
         default='new')
-    
     time = fields.Float(
-        string='Time')
-    
+        string='Time') 
     assigned = fields.Boolean(
         string='Assigned',
         readonly=True)
-
     date_limit = fields.Date(
         string='Date Limit')
-
     action_corrective = fields.Html(
         string='Corrective Action',
         help='Describe corrective actions')
-    
     action_preventive = fields.Html(
         string='Preventive Action',
         help='Describe preventive actions')
+
+    def marcar_asignado(self):
+        self.set_state('asignado')
+
+    def marcar_enproceso(self):
+        self.set_state('proceso')
+
+    def marcar_pendiente(self):
+        self.set_state('pendiente')
+
+    def marcar_resuelto(self):
+        self.set_state('resuelto')
     
-    
-    
-    
-    
-    
+    def marcar_cancelado(self):
+        self.set_state('cancelado')
+
+    def set_state(self, new_state): 
+        self.ensure_one()
+        self.state = new_state
+        self.assigned = True
+
+    def set_reset(self): 
+        self.ensure_one()
+        self.state = 'new'
+        self.assigned = False
+
+
